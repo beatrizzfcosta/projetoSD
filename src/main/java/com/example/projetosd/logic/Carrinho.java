16d4561cbdcf34f4f1bdd20e68a260d442185618
rCodFIsh <elcfsantos@gmail.com>
2025-05-30 18:45:20 +0100

    Security, auth and session management dependency added, Main controller updated to pass products to index.

---
+++ src/main/java/com/example/projetosd/logic/Carrinho.java
@@ -0,0 +1,4 @@
+package com.example.projetosd.logic;
+
+public class Carrinho {
+}
--- src/main/java/com/example/projetosd/repository/ProductRepository.java
+++ src/main/java/com/example/projetosd/repository/ProductRepository.java
@@ -3,5 +3,4 @@ package com.example.projetosd.repository;
 import com.example.projetosd.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-}
+public interface ProductRepository extends CrudRepository<Product, Integer> {}
