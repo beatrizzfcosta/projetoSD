ae5dbd4c99db54f672676002e0988f2cb32b05f2
Kaneki0612 <analfpires@outlook.pt>
2025-06-01 02:19:07 +0100

    item.html, loja.html finalizados

--- src/main/java/com/example/projetosd/controller/ItemController.java
+++ src/main/java/com/example/projetosd/controller/ItemController.java
@@ -18,21 +18,16 @@ public class ItemController {
     private ProductRepository prodRepository;
     @Autowired
     private ProductRepository productRepository;
-    private List<Product> products = new ArrayList<>();
 
     @GetMapping("/{id}")
     public String getItem(@PathVariable Long id, Model model) {
         Product produto = productRepository.getByProductId(id);
-        model.addAttribute("produto", produto);
+        model.addAttribute("product", produto);
         return "item";
     }
 
     @PostMapping("/carrinho")
     public String Btn_addProduct(Model model, Product produto) {
-        products.add(produto);
-        model.addAttribute("listProduts", products);
-
-        System.out.println(products.size());
         return "redirect:/loja";
     }
 }
